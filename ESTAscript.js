b1557795974dad15b3f2eb8b8d1030bbe98c9a9a
FundacionEsta <asociacionetsacorreos@gmail.com>
2025-08-18 05:26:31 -0500

    (ESTA-v1.0.10: se crea nueva interfas propia del curso recreacion con propocito junto a logica de carrusel de imagenes)

--- ESTAscript.js
+++ ESTAscript.js
@@ -12,3 +12,26 @@ function filterCards() {
         card.style.display = (matchesCategory && matchesSearch) ? "block" : "none";
     });
 }
+
+    
+document.getElementById("recreacion").addEventListener("click", function() {
+    window.location.href = "recreacion.html";
+     });
+
+ const imagenes = ["recreacion1.png", "recreacion2.png", "recreacion3.png"]; 
+    let indice = 0;
+
+    const img = document.getElementById("imagen-carrusel");
+    const flechaIzq = document.getElementById("flecha-izq");
+    const flechaDer = document.getElementById("flecha-der");
+
+    flechaIzq.addEventListener("click", () => {
+      indice = (indice - 1 + imagenes.length) % imagenes.length;
+      img.src = imagenes[indice];
+    });
+
+    flechaDer.addEventListener("click", () => {
+      indice = (indice + 1) % imagenes.length;
+      img.src = imagenes[indice];
+    });
+
